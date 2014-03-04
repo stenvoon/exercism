bade854e63afd43f81d01fe62ee39851b6ee9f60
Steve Neely <sneely@rallydev.com>
2014-03-03 20:19:38 -0700

    reformatted to look a little less fugly

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -2,69 +2,54 @@ module.exports = (function() {
 	"use strict";
 
 	var songPattern = "{count} {bottle} of beer on the wall, {count} {bottle} of beer.\n" +
-				"{action}, {oneLess} {oneLessBottle} of beer on the wall.\n";
+				"{action}, {remaining} of beer on the wall.\n";
 
 	var songPatternCompile = function (count) {
-		var song = songPattern.replace(/{count}/g, howMany(count));
-		song = song.replace(/{bottle}/g, pluarlize(count));
-		song = song.replace(/{action}/, doAction(count));
-		song = song.replace(/{oneLess}/, oneLess(count));
-		song = song.replace(/{oneLessBottle}/, pluarlize(count-1))
-		return song.charAt(0).toUpperCase() + song.substring(1);
+		return capitalize(songPattern.replace(/{count}/g, formatCount(count))
+								.replace(/{bottle}/g, pluarlize(count))
+								.replace(/{action}/, formatAction(count))
+								.replace(/{remaining}/, formatRemaining(count)));
 	};
 
-	var doAction = function(count) {
+	var capitalize = function (str) {
+		return str.charAt(0).toUpperCase() + str.substring(1);
+	};
+
+	var formatCount = function(count) {
+		return (count === 0) ? "no more" : count;
+	};
+
+	var formatAction = function(count) {
 		if (count > 0) {
-			return "Take {subject} down and pass it around".replace(/{subject}/, subject(count));
+			return "Take " + (count === 1 ? "it" : "one") + " down and pass it around";
 		}
 		return "Go to the store and buy some more";
 	};
 
-	var howMany = function(count) {
-		if (count === 0) {
-			return "no more";
+	var formatRemaining = function (count) {
+		switch(count) {
+			case 0  : return "99 bottles";
+			case 1  : return "no more bottles";
+			default : return (count - 1) + " " + pluarlize(count - 1);
 		}
-		return count;
-	};
-
-	var oneLess = function (count) {
-		if (count === 0) {
-			return 99;
-		} else if (count === 1) {
-			return "no more";
-		}
-		return count - 1;
 	};
 
 	var pluarlize = function (count) {
-		if (count === 1) {
-			 return "bottle";
-		}
-		return "bottles";
-	};
-
-	var subject = function (count) {
-		if (count === 1) {
-			return "it";
-		}
-		return "one";
+		return (count === 1) ? "bottle" : "bottles";
 	};
 
 	var verse = function (count) {
 		return songPatternCompile(count);
 	};
 
-	var sing = function(startingCount, finishingCount) {
-		if (finishingCount === undefined) {
-			finishingCount = 0;
+	var sing = function(startCount, endCount) {
+		var song = [];
+
+		for (var i = startCount; i >= (endCount || 0); i--) {
+			song.push(verse(i));
 		}
 
-		var song = "";
-		while (startingCount >= finishingCount) {
-			song = song + verse(startingCount) + "\n";
-			startingCount--;
-		}
-		return song.substring(0, song.length-1);
+		return song.join("\n");
 	};
 
 	return { verse : verse , sing : sing };
