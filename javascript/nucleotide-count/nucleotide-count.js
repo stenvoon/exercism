ff806492e5973fc28bccb36bcfe98f00b658a4bf
Steve Neely <sneely@rallydev.com>
2014-03-16 08:46:28 -0600

    jshint cleanuo

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -2,23 +2,23 @@ module.exports = function DNA(sequence) {
 	"use strict";
 
 	var nucleotideCounts = countNucleotides(sequence),
-		 validNucleotides = "ATCGU";
+		validNucleotides = "ATCGU";
 
 	function calculateCounts (currentCounts, value) {
 		currentCounts[value]++;
 		return currentCounts;
-	};
+	}
 
 	function countNucleotides (sequence) {
 		return sequence
-				 .split('')
-				 .reduce(calculateCounts, { A : 0, T : 0, C : 0, G : 0 });
-	};
+				.split('')
+				.reduce(calculateCounts, { A : 0, T : 0, C : 0, G : 0 });
+	}
 
 	function count (nucleotide) {
 		if (validNucleotides.indexOf(nucleotide) === -1) throw new Error("Invalid Nucleotide");
 		return nucleotideCounts[nucleotide] || 0;
-	};
+	}
 
 	return { 
 		nucleotideCounts : nucleotideCounts, 
