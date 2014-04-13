1c90e9747ae31ac32ac4c6524622a88cc496970f
Steve Neely <sneely@rallydev.com>
2014-04-13 09:44:51 -0600

    clean up

--- javascript/point-mutations/point-mutations.js
+++ javascript/point-mutations/point-mutations.js
@@ -1,18 +1,15 @@
 module.exports = function(sequence) {
 
 	function shortest(a, b) {
-		return a.length < b.length ? a : b;
+		return a.length < b.length ? a.length : b.length;
 	}
 
 	this.hammingDistance = function(input){
 		var distance = 0;
-		var shorterString = shortest(input, sequence);
-		var longerString = (shorterString === input ? sequence : input);
+		var lengthOfShortestString = shortest(input, sequence);
 
-		for (var i = 0; i < shorterString.length; i++) {
-			if (shorterString[i] !== longerString[i]) {
-				distance++;
-			}
+		for (var i = 0; i < lengthOfShortestString; i++) {
+			if (sequence[i] !== input[i]) { distance++; }
 		}
 		return distance;
 	};
