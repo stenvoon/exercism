71bfd319bc8cb9ebab0fd7b121fa56ad42fcba6e
Steve Neely <sneely@rallydev.com>
2014-04-13 11:33:23 -0600

    More refactoring

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,17 +1,21 @@
 module.exports = function(number) {
 	"use strict";
 
+	function isInvalid(number){
+		return number.length < 10 || (number.length == 11 && number[0] !== '1');
+	}
+
 	function clean(number){
 		var cleaned = number.replace(/[\(\)-\s\.]/g, '');
 
-		if (cleaned.length == 11) {
-			return cleaned[0] === '1' ? cleaned.substr(1) : "0000000000";
-		}
-
-		if (cleaned.length < 10) {
+		if (isInvalid(cleaned)) {
 			return "0000000000";
 		}
-		
+
+		if (cleaned.length == 11) {
+			return cleaned.substr(1);
+		}
+
 		return cleaned;
 	}
 
