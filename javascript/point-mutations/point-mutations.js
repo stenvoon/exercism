9897920e2f243fc039bc1e6e77d5066557477cc7
Steve Neely <sneely@rallydev.com>
2014-04-13 09:36:11 -0600

    All tests passing

---
+++ javascript/point-mutations/point-mutations.js
@@ -0,0 +1,20 @@
+module.exports = function(sequence) {
+
+	function shortest(a, b) {
+		return a.length < b.length ? a : b;
+	}
+
+	this.hammingDistance = function(input){
+		var distance = 0;
+		var shorterString = shortest(input, sequence);
+		var longerString = (shorterString === input ? sequence : input);
+
+		for (var i = 0; i < shorterString.length; i++) {
+			if (shorterString.charAt(i) !== longerString.charAt(i)) {
+				distance++;
+			}
+		}
+		return distance;
+	};
+};
+
